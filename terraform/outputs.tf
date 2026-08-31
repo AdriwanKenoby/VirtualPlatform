63a7e187aeeb212bec5b8be43f1e1176d4e01c18
AdriwanKenoby <adri17ve@gmail.com>
2026-08-31 14:13:04 +0200

    add apt repository k8s and cri-o

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,3 @@
-output "network_info" {
-  description = "Informations sur les VMs créées"
-  value = libvirt_network.virbr
-}
-
-output "test" {
-  description = "test"
-  value = data.libvirt_domain_interface_addresses.ips_addresses
-}
 output "vms_ips" {
   description = "VM ips associated"
   value = [
